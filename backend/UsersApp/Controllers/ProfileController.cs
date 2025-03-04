7f45c94f8826c63b02d9b5cb8ce2dbf885d09e7e
YuseinToslev <yusitoslev@gmail.com>
2025-03-04 22:10:43 +0200

    добавяне на регистър

---
+++ backend/UsersApp/Controllers/ProfileController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using UsersApp.Models;
+using UsersApp.ViewModels;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace UsersApp.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+    }
+}
